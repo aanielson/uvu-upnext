476135b639ec98b6eb24c82e097e568e1ad578db
Abigail Nielson <aa.nielson18@gmail.com>
2021-07-19 21:28:28 -1000

    database work

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,24 @@ DROP TABLE IF EXISTS events;
 CREATE TABLE events (
     id int NOT NULL AUTO_INCREMENT,
     event_name varchar(255) NOT NULL,
+    event_date DATETIME NOT NULL,
     event_description varchar(1000) NOT NULL,
-    upcoming BOOL DEFAULT false,
+    upcoming BOOL DEFAULT TRUE,
+    PRIMARY KEY (id)
+);
+
+CREATE DATABASE IF NOT EXISTS upnext_db;
+USE upnext_db;
+
+-- If the table already exists, remove it before trying to create the table again
+DROP TABLE IF EXISTS upNext;
+
+-- Create the events table
+CREATE TABLE upNext (
+    id int NOT NULL AUTO_INCREMENT,
+    title varchar(255) NOT NULL,
+    posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    summary varchar(255) NOT NULL,
+    link varchar(15) NOT NULL,
     PRIMARY KEY (id)
 );
