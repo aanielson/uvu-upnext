9d24850d1d379daa58a9c945d89c67941346b62b
Abigail Nielson <aa.nielson18@gmail.com>
2021-07-20 11:22:29 -1000

    server testing

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,8 @@ var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "upNext_db"
+  database: "upNext_db",
+  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 });
 
 connection.connect(function(err) {
