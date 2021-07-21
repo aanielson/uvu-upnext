b8c757c1352c00f45a0e997c4d1e0a250b605898
Abigail Nielson <aa.nielson18@gmail.com>
2021-07-20 18:13:31 -1000

    handlebars basics

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+import * as Handlebars from 'handlebars';
+import * as path from 'path';
+
+const partial_path = path.join(HBS, partial);
+const { name } = path.parse(path.basename(partial_path));
+Handlebars.registerPartial(name, readHbs(partial_path)); 
